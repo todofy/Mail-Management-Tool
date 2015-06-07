a2a3a486f52b5a8de4ce38a2e8cf161702c52bca
Anshuman <anshumanpattanayak@gmail.com>
2015-06-07 13:21:27 +0530

    Header n sidebar files made
    From template. Sidebar doesn't function as of now.

---
+++ dashboard/config/config.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * dummy config file 
+ * this is the format of config file that shall be used by
+ * all data here shall be taken by user at the time of installation
+ */
+ if(!isset($checkVar)) 
+ {
+    //header("location: ../../index.php");
+    exit;
+ }
+define("HOST","localhost");
+define("USER","root");
+define("PASSWORD","");
+define("_DB_MAIN","remember_me");
