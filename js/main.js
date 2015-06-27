6ef2148e4139a29af5e98e4cbc7fee2f6b433dcd
Minhaz A V <minhazav@gmail.com>
2015-06-28 04:10:48 +0530

    AJAX Framework js code added

---
+++ js/main.js
@@ -0,0 +1,44 @@
+/**
+ * Js file to serve major codes, to be included everywhere
+ */
+
+var AJAX = function(url, category, data, successCallback, failureCallback) {
+	this.url = url;
+	this.data = data;
+	this.category = category;
+	this.successCallback = successCallback;
+	this.failureCallback = failureCallback;
+
+	this.trigger();
+}
+
+AJAX.prototype.trigger = function() {
+	var XHR = new XMLHttpRequest();
+	XHR.open('POST', this.url);
+	XHR.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+
+	var _this = this;
+	XHR.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			try {
+				var obj = JSON.parse(this.response);
+				if (obj.error) {
+					alert(obj.message);
+					if (typeof _this.failureCallback  == 'function') _this.failureCallback(obj);
+				} else {
+					if (typeof _this.successCallback  == 'function') _this.successCallback(obj);
+				}
+			} catch (ex) {
+				// TODO connect this to a notification later
+				alert('Unable to parse response from server. Invalid Response From Server');
+				if (typeof _this.failureCallback  == 'function') _this.failureCallback();
+			}
+		} else if (this.readyState == 4) {
+			// TODO connect this to a notification later
+			alert('Unable to connect to internet');
+		}
+	}
+
+
+	XHR.send('data=' +JSON.stringify({category: this.category, data: this.data}));
+};
