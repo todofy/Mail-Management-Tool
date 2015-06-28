2e9c2a23c5299764780e6e2594e56d2c49c18bfc
ashish-pandey <apndey.15@gmail.com>
2015-06-28 23:26:04 +0530

    working edit_admin

--- ajaxserver/edit_admin.php
+++ ajaxserver/edit_admin.php
@@ -7,11 +7,11 @@
 	if(!empty($result)){
 		$id = $result[0]['id'];
 		//delete previous accesses of admin
-    	$result = database::SQL("DELETE FROM acl WHERE admin_id = ? ",array('s',$id));
+    	$result = database::SQL("DELETE FROM admin_access WHERE admin_id = ? ",array('i',$id));
     	//insert the new accesses
     	if(!empty($access) && is_array($access))
 	    	foreach ($access as $value){
-	    		$result = database::SQL("INSERT into admin_access values (?,?)",array('ss',$id,$value));
+	    		$result = database::SQL("INSERT into admin_access values (?,?)",array('ii',$id,$value));
 	    	}
 	    $output['error'] = false;
 		$output['message'] = 'Successfully edited.';
