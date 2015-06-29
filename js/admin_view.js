f09a3235342e87f9558970b72b30a811d4756065
ashish-pandey <apndey.15@gmail.com>
2015-06-29 18:42:55 +0530

    working delete_admin
    there are still some fixes to be done

--- js/admin_view.js
+++ js/admin_view.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-  	$(".btn-danger").click(function(e){
+  	$(".btn.btn-danger").click(function(e){
 	    //get id of button clicked
         var id = this.id;
         console.log(id);
-        //request = new AJAX ('delete_admin', {admin_id : id} , function(d){alert('Success.')}, function(d){alert('Failure.')});
+        request = new AJAX ('delete_admin', {admin_id : id} , function(d){alert('Success.')}, function(d){alert('Failure.')});
         e.preventDefault();	
 		});
 });
