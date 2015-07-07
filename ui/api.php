b7d3f18a9ecc37976155d0640dc9d673cd9c74c4
Anshuman <anshumanpattanayak@gmail.com>
2015-07-07 16:23:22 +0530

    Create API UI added.

---
+++ ui/api.php
@@ -0,0 +1,51 @@
+<?php
+if (!isset($_CODE_API)) {
+    throw new Exception("Code file not included for api.php!");
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+
+	<!-- jQuery library -->
+	<script src="./js/jquery-2.0.0.min.js"></script>
+
+	<!-- Latest compiled JavaScript -->
+	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="css/jAlert-v3.css" />
+</head>
+
+<body>
+
+	<?php
+	include __DIR__ .'/header.php';
+	include __DIR__ .'/sidebar.php';
+	?>
+
+	<!-- Main workspace starts from here -->
+	<div id="content-wrapper">
+		<div class="row">
+			<ol class="breadcrumb">
+			    <li><a href="./dashboard.php">Home</a></li>
+			    <li class="active">API</li>
+			</ol>
+		</div>
+		<div class="row">
+		    <div class="col-md-9">
+		    	<h3>View APIs</h3>
+		    </div>
+		    <div class="col-md-3">
+				<a href="api_add.php" type="button" class="btn btn-primary pull-right">Add</a>
+			</div>
+		</div>
+	</div>
+<script src="js/jAlert-v3.js"></script>
+<script src="js/jAlert-functions.js"></script>
+</body>
+</html>
---
+++ ui/api_add.php
@@ -0,0 +1,61 @@
+<?php
+if (!isset($_API_ADD)) {
+    throw new Exception("Code file not included for api_add.php!");
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+
+	<!-- jQuery library -->
+	<script src="./js/jquery-2.0.0.min.js"></script>
+
+	<!-- Latest compiled JavaScript -->
+	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="css/jAlert-v3.css" />
+</head>
+
+<body>
+
+	<?php
+	include __DIR__ .'/header.php';
+	include __DIR__ .'/sidebar.php';
+	?>
+
+	<!-- Main workspace starts from here -->
+	<div id="content-wrapper">
+		<div class="row">
+			<ol class="breadcrumb">
+			    <li><a href="./dashboard.php">Home</a></li>
+			    <li><a href="template.php">API</a></li>
+			    <li class="active">Add</li>
+			</ol>
+		</div>
+		<div class="row">
+			<h3>Create API</h3>
+			<form role="form" type="post" id="create-template">
+				<div class="form-group">
+    				<label for="template-name"><h4>API Name</h4></label>
+    				<input type="text" class="form-control" id="template-name" name="template-name" style="width: 40%; min-width: 200px">
+  				</div>
+  				<div class="form-group">
+  					<label class="control-label"><h4>Select Template</h4></label>
+  					<select class="form-control" style="width:auto; display:inline-block; margin-left: 10px;">
+    					<option>Templates</option>
+  					</select>
+  					<a href="template_add.php" type="button" class="btn btn-link">Create a new Template</a>
+				</div>
+			</form>
+  			<button type="submit" class="btn btn-primary" value="Submit" id="btn">Save</button>
+		</div>
+	</div>
+<script src="js/jAlert-v3.js"></script>
+<script src="js/jAlert-functions.js"></script>
+</body>
+</html>
