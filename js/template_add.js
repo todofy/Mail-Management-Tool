ea88f7ea627fcaa95fb93fcd71f0ee8fd378b157
ashish-pandey <apndey.15@gmail.com>
2015-07-08 20:37:13 +0530

    form validation in add template

--- js/template_add.js
+++ js/template_add.js
@@ -1,31 +1,56 @@
 $(document).ready(function() {
   	$("#create").click(function(e){
-  	 		  var jsonData = {};
-          //get html content from editor
-          var text = tinyMCE.activeEditor.getContent();
-          $("#template-text").val(text);
-  	 		  var formData = $("#create-template").serializeArray();
-     
-     			$.each(formData, function() {
-          		if (jsonData[this.name]) {
-             			if (!jsonData[this.name].push) {
-                 			jsonData[this.name] = [jsonData[this.name]];
-             			}
-             			jsonData[this.name].push(this.value || '');
-         			} else {
-             			jsonData[this.name] = this.value || '';
-         			}
-  	    	});
-     			//console.log(jsonData);
-          request = new AJAX ('add_template', jsonData, function(d){
-              successAlert(d.message);
-              $(".closejAlert.ja_close").click(function(e){
-                  window.location="template.php";
-                  e.preventDefault();
-                });
-              }, function(d){
-              errorAlert(d.message);
-              }); 
-          e.preventDefault(); 		    
+          if(check_template())
+          {
+              var jsonData = {};
+            //get html content from editor
+            var text = tinyMCE.activeEditor.getContent();
+            $("#template-text").val(text);
+            var formData = $("#create-template").serializeArray();
+       
+            $.each(formData, function() {
+                if (jsonData[this.name]) {
+                    if (!jsonData[this.name].push) {
+                        jsonData[this.name] = [jsonData[this.name]];
+                    }
+                    jsonData[this.name].push(this.value || '');
+                } else {
+                    jsonData[this.name] = this.value || '';
+                }
+            });
+            //console.log(jsonData);
+            request = new AJAX ('add_template', jsonData, function(d){
+                successAlert(d.message);
+                $(".closejAlert.ja_close").click(function(e){
+                    window.location="template.php";
+                    e.preventDefault();
+                  });
+                }, function(d){
+                errorAlert(d.message);
+                }); 
+            e.preventDefault();        
+          }
+          else
+            return false;
+  	 		   
   	  });
 });
+
+function check_template()
+{
+  var temp_text = $('#template-text').val();
+  var name  = $('#template-name').val();
+  if(temp_text=='')
+  {
+    alert('template text empty');
+  }
+  else if($.trim(name)=='')
+  {
+    alert('provide a template name');
+  }
+  else
+  {
+    return true;
+  }
+  return false;
+}
