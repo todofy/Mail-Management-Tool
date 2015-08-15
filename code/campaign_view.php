9a262e939c1956a94a4bc371565447ad8ba44d53
ashish-pandey <apndey.15@gmail.com>
2015-08-15 15:00:37 +0530

    conflict corrected

--- code/campaign_view.php
+++ code/campaign_view.php
@@ -15,12 +15,8 @@
 		$error = true;
 	else{
 		//get all mails for specific campaign
-<<<<<<< HEAD
 		$mails = database::SQL("SELECT `mail`.`id`, `description`, `status`, `time_started`, `time_finished` FROM `mail`,`mail_status` WHERE `campaign_id`=? AND `mail`.`status`=`mail_status`.`type`",array('s',$campaign_id));
 		
-=======
-		$mails = database::SQL("SELECT `mail`.`id`, `description`, `status`, `time_started`, `time_finished`, SUM(`clicks`) as `clicks` FROM `mail`,`mail_status`,`link_hash` WHERE `campaign_id`=? AND `mail`.`status`=`mail_status`.`type` AND `mail`.`id`=`link_hash`.`mail_id` GROUP BY `link_hash`.`mail_id`",array('s',$campaign_id));
->>>>>>> origin/master
 	}
 	$_CAMPAIGN_VIEW_ = true;
 
