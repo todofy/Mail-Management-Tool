d8526837e142bf79e641e3f23af31dcdbfc42225
ashish-pandey <apndey.15@gmail.com>
2015-08-15 18:27:23 +0530

    small changes to click count

--- code/campaign_view.php
+++ code/campaign_view.php
@@ -16,7 +16,18 @@
 	else{
 		//get all mails for specific campaign
 		$mails = database::SQL("SELECT `mail`.`id`, `description`, `status`, `time_started`, `time_finished` FROM `mail`,`mail_status` WHERE `campaign_id`=? AND `mail`.`status`=`mail_status`.`type`",array('s',$campaign_id));
-		
+		for($i=0 ; $i< count($mails) ; $i++)
+		{
+			$result = database::SQL("SELECT SUM(`clicks`) AS `clicks` FROM `link_hash` WHERE `mail_id` = ? " , array('i' , $mails[$i]['id']));
+			if(!isset($result[0]['clicks']))
+			{
+				$mails[$i]['clicks'] = '-';
+			}
+			else
+			{
+				$mails[$i]['clicks'] = $result[0]['clicks'];
+			}
+		}
 	}
 	$_CAMPAIGN_VIEW_ = true;
 
