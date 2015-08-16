08b37f535b22791fbfd36cd8cca5a6390c7ea36c
Anshuman <anshumanpattanayak@gmail.com>
2015-08-16 11:53:44 +0530

    Search update complete.

---
+++ js/search.js
@@ -0,0 +1,90 @@
+$(document).ready(function() {
+
+    $(".button-delete-admin").on('click', function(e) {
+    	//get id of button clicked
+        var id = $(this).attr('id');
+        var _this = $(this);
+        $(".button-delete-confirm-admin").on('click',function(e){
+            request = new AJAX ('delete_admin', 
+                {admin_id : id},
+                function(d) {
+                    successAlert('Admin deleted successfully.');
+                    _this.parent('td').parent('tr').remove();
+                }, function(d){
+                    errorAlert('Unable to delete admin.');
+                });
+
+            $(this).unbind("click");
+            e.preventDefault();
+        });
+        e.preventDefault();
+    });
+
+
+    $(".button-revoke-admin").on('click', function(e){
+        //get id of button clicked
+        var id = $(this).attr('id');       
+        $(".button-revoke-confirm-admin").on('click',function(){
+    	   request = new AJAX ('revoke_admin', {admin_id : id} ,
+                function(d){
+                successAlert('Admin API Key revoked! New API Secret key is ' +d.data +'<br>A mail has been sent to admin with new API key!');
+                }, function(d){
+                    errorAlert('Unable to revoke Secret API key for this admin!');
+            });
+            $(this).unbind("click");
+        });
+        e.preventDefault();	
+	});
+
+    $(".button-delete-api").on('click', function(e) {
+        //get id of button clicked
+        var id = $(this).attr('id');
+        var _this = $(this);
+        $(".button-delete-confirm-api").on('click',function(e){
+            request = new AJAX ('delete_api', 
+                {api_id : id},
+                function(d) {
+                    successAlert(d.message);
+                    _this.parent('td').parent('tr').remove();
+                }, function(d){
+                    errorAlert(d.message);
+                });
+            $(this).unbind("click");
+            e.preventDefault();
+        });
+        e.preventDefault();
+    });
+
+    $(".button-view-template").on('click', function(e) {
+        //get id of button clicked
+        var id = $(this).attr('id');
+        request = new AJAX ('view_template', 
+            {template_id : id},
+            function(d) {
+                $("#template-preview").html(d.data);
+                $(".button-edit-secondary").attr("href", "template_edit.php?id="+id);
+            }, function(d){
+                $("#template-preview").html(d.data);
+        });
+        e.preventDefault();
+
+    });
+    $(".button-delete-template").on('click', function(e) {
+        //get id of button clicked
+        var id = $(this).attr('id');
+        var _this = $(this);
+        $(".button-delete-confirm-template").on('click',function(e){
+            request = new AJAX ('delete_template', 
+                {template_id : id},
+                function(d) {
+                    successAlert(d.message);
+                    _this.parent('td').parent('tr').remove();
+                }, function(d){
+                    errorAlert(d.message);
+                });
+            $(this).unbind("click");
+            e.preventDefault();
+        });
+        e.preventDefault();
+    });
+});
--- ui/search.php
+++ ui/search.php
@@ -69,11 +69,11 @@ if (!isset($_SEARCH_VIEW_)) {
 								}
 								else echo '<a class="btn btn-primary disabled button-edit" href="javascript:void(0)" role="button">Edit</a> ';
 								if (isset($newuser->access[REVOKE_ADMIN]) && $value['id']!=$id) {
-									echo '<a class="btn btn-warning button-revoke" href="#" role="button" data-toggle="modal" data-target="#revoke" id='.$value['id'].'>Revoke key</a> ';
+									echo '<a class="btn btn-warning button-revoke-admin" href="#" role="button" data-toggle="modal" data-target="#revoke-admin" id='.$value['id'].'>Revoke key</a> ';
 								}
 								else echo '<a class="btn btn-warning disabled button-edit" href="javascript:void(0)" role="button">Revoke key</a> ';
 								if (isset($newuser->access[DELETE_ADMIN]) && $value['id']!=$id) {
-									echo '<a class="btn btn-danger button-delete" href="#" role="button" data-toggle="modal" data-target="#delete" id='.$value['id'].'>Delete</a> ';
+									echo '<a class="btn btn-danger button-delete-admin" href="#" role="button" data-toggle="modal" data-target="#delete-admin" id='.$value['id'].'>Delete</a> ';
 								}
 								else echo '<a class="btn btn-danger disabled button-edit" href="javascript:void(0)" role="button">Delete</a> ';
 								echo '</td>';
@@ -145,7 +145,7 @@ if (!isset($_SEARCH_VIEW_)) {
 									echo '<a class="btn btn-info button-view" href='.$view.' role="button" id='.$value['id'].'>View</a> ';
 									$edit = "api_edit.php?id=".$value['id'];
 									echo '<a class="btn btn-primary button-edit" href='.$edit.' role="button" id='.$value['id'].'>Edit</a> ';
-									echo '<a class="btn btn-danger button-delete" href="#" role="button" data-toggle="modal" data-target="#delete" id='.$value['id'].'>Delete</a> ';
+									echo '<a class="btn btn-danger button-delete-api" href="#" role="button" data-toggle="modal" data-target="#delete-api" id='.$value['id'].'>Delete</a> ';
 								echo '</td>';
 								echo '</tr>';
 							}							
@@ -157,7 +157,7 @@ if (!isset($_SEARCH_VIEW_)) {
 				echo '
 					<div class="row">
 					<div class="col-md-12">
-						<h3>In Templatess</h3>
+						<h3>In Templates</h3>
 						<table class="table">
 							<tr style="background-color: #E0E0E0;">
 							<th class="col-md-1"><center>#ID</center></th>
@@ -177,10 +177,10 @@ if (!isset($_SEARCH_VIEW_)) {
 								echo '<td><center>' .date("D, d M 20y", $value['created_on']).'<br>'.date("h:i:s A (e)", $value['created_on']).'</center></td>';
 								echo '<td><center>' .date("D, d M 20y", $value['last_updated']).'<br>'.date("h:i:s A (e)", $value['last_updated']).'</center></td>';
 								echo '<td class="pull-right">';
-									echo '<a class="btn btn-info button-view" href="#" role="button" data-toggle="modal" data-target="#preview" id='.$value['id'].'>View</a> ';
+									echo '<a class="btn btn-info button-view-template" href="#" role="button" data-toggle="modal" data-target="#preview" id='.$value['id'].'>View</a> ';
 									$edit = "template_edit.php?id=".$value['id'];
 									echo '<a class="btn btn-primary button-edit" href='.$edit.' role="button" id='.$value['id'].'>Edit</a> ';
-									echo '<a class="btn btn-danger button-delete" href="#" role="button" data-toggle="modal" data-target="#delete" id='.$value['id'].'>Delete</a> ';
+									echo '<a class="btn btn-danger button-delete-template" href="#" role="button" data-toggle="modal" data-target="#delete" id='.$value['id'].'>Delete</a> ';
 								echo '</td>';
 								echo '</tr>';
 								}							
@@ -190,6 +190,86 @@ if (!isset($_SEARCH_VIEW_)) {
 			}										
 		?>
 	</div>
+	<div id="revoke-admin" class="modal fade" role="dialog" style="z-index: 15000; margin-top:100px;">
+  			<div class="modal-dialog">
+    			<div class="modal-content">
+    				<div class="modal-header">
+        				<h4 class="modal-title">Revoke Secret Key</h4>
+      				</div>
+      				<div class="modal-body">
+        				<p>Are you sure you want to revoke the secret key for this admin?</p>
+        				<br>
+        				<button type="button" class="btn btn-warning button-revoke-confirm-admin" data-dismiss="modal">Revoke key</button>
+        				<button type="button" class="btn btn-link" data-dismiss="modal">Cancel</button>
+        			</div>
+    			</div>
+  			</div>
+		</div>
+		<div id="delete-admin" class="modal fade" role="dialog" style="z-index: 15000; margin-top:100px;">
+  			<div class="modal-dialog">
+    			<div class="modal-content">
+    				<div class="modal-header">
+        				<h4 class="modal-title">Delete Admin</h4>
+      				</div>
+      				<div class="modal-body">
+        				<p>Are you sure you want to delete this admin?</p>
+        				<br>
+        				<button type="button" class="btn btn-danger button-delete-confirm-admin" data-dismiss="modal">Delete</button>
+        				<button type="button" class="btn btn-link" data-dismiss="modal">Cancel</button>
+        			</div>
+    			</div>
+  			</div>
+		</div>
+		<div id="delete-api" class="modal fade" role="dialog" style="z-index: 15000; margin-top:100px;">
+  			<div class="modal-dialog">
+    			<div class="modal-content">
+    				<div class="modal-header">
+        				<h4 class="modal-title">Delete API</h4>
+      				</div>
+      				<div class="modal-body">
+        				<p>Are you sure you want to delete this API?</p>
+        				<br>
+        				<button type="button" class="btn btn-danger button-delete-confirm-api" data-dismiss="modal">Delete</button>
+        				<button type="button" class="btn btn-link" data-dismiss="modal">Cancel</button>
+        			</div>
+    			</div>
+  			</div>
+		</div>	
+		<div id="preview" class="modal fade" role="dialog" style="z-index: 15000; margin-top:40px;">
+  			<div class="modal-dialog" style="width: 950px; min-width:800px; margin-left: 260px; overflow-y: initial;">
+    			<div class="modal-content">
+    				<div class="modal-header">
+        				<h4 class="modal-title">Preview Template</h4>
+      				</div>
+      				<div class="modal-body"style="overflow-y: auto;">
+      					<div class="well" id="template-preview" ></div>
+      				</div>
+  					<div class="modal-footer" style="margin-top: -20px;">
+      					<a class="btn btn-primary button-edit-secondary" href='#' role="button">Edit</a>
+        				<button type="button" class="btn btn-link" data-dismiss="modal">Back</button>
+        			</div>
+    			</div>
+  			</div>
+		</div>
+		<div id="delete" class="modal fade" role="dialog" style="z-index: 15000; margin-top:100px;">
+  			<div class="modal-dialog">
+    			<div class="modal-content">
+    				<div class="modal-header">
+        				<h4 class="modal-title">Delete Template</h4>
+      				</div>
+      				<div class="modal-body">
+        				<p>Are you sure you want to delete this template?</p>
+        				<br>
+        				<button type="button" class="btn btn-danger button-delete-confirm-template" data-dismiss="modal">Delete</button>
+        				<button type="button" class="btn btn-link" data-dismiss="modal">Cancel</button>
+        			</div>
+    			</div>
+  			</div>
+		</div>	
 <script src="js/dashboard.js"></script>
+<script src="js/jAlert-v3.js"></script>
+<script src="js/jAlert-functions.js"></script>
+<script src="js/main.js"></script>
+<script src="js/search.js"></script>
 </body>
 </html>
