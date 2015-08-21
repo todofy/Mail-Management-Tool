98b5b99815f37f1affa4aac4547da4bb7bf512f8
Anshuman <anshumanpattanayak@gmail.com>
2015-08-21 06:45:12 +0530

    Login testing #2

--- code/index.php
+++ code/index.php
@@ -12,7 +12,7 @@ if(isset($_GET['err'])) $err = $_GET['err'];
 if(isset($_POST['commit'])) {
     $email = $_POST['login'];
     $password = $_POST['password'];
-
+    echo "Now";
     if (login::login_user($email, $password, isset($_POST['remember_me']))) {
     	echo "Here";
         redirect_to("dashboard.php");
@@ -20,6 +20,6 @@ if(isset($_POST['commit'])) {
     echo "There";
     $err = "Incorrect Email or Password";
 }
-
+echo "Nowhere";
 $_CODE_INDEX_ = true;
 ?>
