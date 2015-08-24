2a208a06e06db2c2545c906abff7f4623626e0c6
Anshuman <anshumanpattanayak@gmail.com>
2015-08-25 00:14:37 +0530

    Sidebar fixed

--- ui/sidebar.php
+++ ui/sidebar.php
@@ -8,43 +8,45 @@ if (!isset($_CODE_SIDEBAR_)) {
 <link href="css/sidebar.css" rel="stylesheet" type="text/css">
 
 
-<div id="wrapper" class="col-xs-2 col-md-2">
-        <div id="sidebar-wrapper" class="col-xs-2 col-md-2">
-            <ul id="sidebar_menu" class="sidebar-nav sidebar-name">
-                <li class="sidebar-nav sidebar-brand"><a id="menu-toggle" href="javascript:void(0)" style="pointer-events:none;">Menu<span id="main_icon" class="sub_icon"></span></a></li>
-            </ul>
-            <ul class="sidebar-nav" id="sidebar">
-                  <li>
-                      <a href="dashboard.php" style="font-size:15px;">
-                         Dashboard<span class="sub_icon glyphicon glyphicon-home"></span>
-                      </a>
-                  </li>
-                  <li>
-                      <a href="#" style="font-size:15px;">
-                         Send Mail<span class="sub_icon glyphicon glyphicon-envelope"></span>
-                      </a>
-                  </li>
-                  <li>
-                      <a href="template.php" style="font-size:15px;">
-                         Template<span class="sub_icon glyphicon glyphicon-pencil"></span>
-                      </a>
-                  </li> 
-                  <li>
-                      <a href="api.php" style="font-size:15px;">
-                         API<span class="sub_icon glyphicon glyphicon-file"></span>
-                      </a>
-                  </li> 
-                  <?php 
-                      foreach ($newuser->access as $row) {
-                          if ($row['link'] == '') continue;
-                          echo"<li><a href=\"{$row['link']}\">{$row['display_name']}<span class='sub_icon glyphicon glyphicon-user'></span></a></li>";
-                      } 
-                  ?> 
-            </ul>
-        </div> 
-   
+<div id="wrapper" class="col-xs-2">
+    <div id="sidebar-wrapper" class="col-xs-2">
+        <div class="col-xs-12" style="padding-left:0px; padding-right:0px;">
+          <ul id="sidebar_menu" class="sidebar-nav sidebar-name">
+              <li class="sidebar-nav sidebar-brand"><a id="menu-toggle" href="javascript:void(0)" style="pointer-events:none;">Menu<span id="main_icon" class="sub_icon"></span></a></li>
+          </ul>
+        </div>
+        <div class="col-xs-12" style="padding-left:0px; padding-right:0px;">
+          <ul class="sidebar-nav" id="sidebar">
+                <li>
+                    <a href="dashboard.php" style="font-size:15px;">
+                       Dashboard<span class="sub_icon glyphicon glyphicon-home"></span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" style="font-size:15px;">
+                       Send Mail<span class="sub_icon glyphicon glyphicon-envelope"></span>
+                    </a>
+                </li>
+                <li>
+                    <a href="template.php" style="font-size:15px;">
+                       Template<span class="sub_icon glyphicon glyphicon-pencil"></span>
+                    </a>
+                </li> 
+                <li>
+                    <a href="api.php" style="font-size:15px;">
+                       API<span class="sub_icon glyphicon glyphicon-file"></span>
+                    </a>
+                </li> 
+                <?php 
+                    foreach ($newuser->access as $row) {
+                        if ($row['link'] == '') continue;
+                        echo"<li><a href=\"{$row['link']}\">{$row['display_name']}<span class='sub_icon glyphicon glyphicon-user'></span></a></li>";
+                    } 
+                ?> 
+          </ul>
+        </div>
+    </div>    
 </div>
-
 
 <!-- TODO: do something such that these scripts are always included at the bottom -->
 <script src="js/sidebar.js"></script>
