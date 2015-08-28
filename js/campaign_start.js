855493d324185b1ed3aceac65d580d48323de440
ashish-pandey <apndey.15@gmail.com>
2015-08-28 20:29:55 +0530

    validation added

--- js/campaign_start.js
+++ js/campaign_start.js
@@ -1,28 +1,74 @@
 $(document).ready(function(){ 
 	$("#start").click(function(e){
- 		var jsonData = {};
-        formData = form2js('start_campaign', '.', true,
-                function(node)
-                {
-                    if (node.id && node.id.match(/callbackTest/))
+        if(validate_form())
+        {
+     		var jsonData = {};
+            formData = form2js('start_campaign', '.', true,
+                    function(node)
                     {
-                        return { name: node.id, value: node.innerHTML };
-                    }
-                });
-        jsonData = JSON.stringify(formData, null, '\t');
+                        if (node.id && node.id.match(/callbackTest/))
+                        {
+                            return { name: node.id, value: node.innerHTML };
+                        }
+                    });
+            jsonData = JSON.stringify(formData, null, '\t');
 
-        request = new AJAX ('campaign_start', jsonData, function(d){
-                successAlert(d.message);
-                $(".closejAlert.ja_close").click(function(e){
-                    window.location="dashboard.php";
-                    e.preventDefault();
-                });
-                }, function(d){
-                    errorAlert(d.message);
+            request = new AJAX ('campaign_start', jsonData, function(d){
+                    successAlert(d.message);
                     $(".closejAlert.ja_close").click(function(e){
-                        window.location="campaign_create.php";
+                        window.location="dashboard.php";
                         e.preventDefault();
                     });
-                });
+                    }, function(d){
+                        errorAlert(d.message);
+                        $(".closejAlert.ja_close").click(function(e){
+                            window.location="campaign_create.php";
+                            e.preventDefault();
+                        });
+                    });
+        }
 	});
+      
 });
+
+function validate_form()
+{
+    /*
+        .addClass("has-error");
+    */
+    $('.form-control').each(function(){
+        console.log($(this).val());
+        var curId = $(this).attr('id');
+        if($.trim($(this).val())=="")
+        {
+            console.log(curId);
+            if(curId!="search_key")
+            {
+                $(this).css('border-color' , 'red');
+                success = false;
+            }
+        }
+        else
+        {
+            if(curId=="to")
+            {
+                //validate the email
+                var email= $(this).val();
+                var pattern  = /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
+                if(!pattern.test(email)){
+                  $(this).css('border-color' , 'red');;
+                  success = false;
+                }
+                else{
+                    $(this).css('border-color' , 'white');
+                }
+            }
+            else
+            {
+                $(this).css('border-color' , 'white');
+            }
+                
+        }
+    }); 
+    return false;
+}
