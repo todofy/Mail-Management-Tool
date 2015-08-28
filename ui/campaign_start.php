9271839353ba665c52e38b8da93cdd025a8440e3
Anshuman <anshumanpattanayak@gmail.com>
2015-08-28 19:41:50 +0530

    Check

--- ui/campaign_start.php
+++ ui/campaign_start.php
@@ -45,7 +45,7 @@ if (!isset($_CAMPAIGN_START_)) {
                 {
                   echo '
                   <div style="background-color: #F5F5F5; padding: 10px; margin-bottom: 10px; border-radius: 10px; border: 1px solid #B9B9B9;">
-                    <h3 style="float:right; margin-top:0px;">#'.($i+1).'</h3>    
+                    <h3 style="margin-bottom:0px; margin-top:0px; text-align: right;">#'.($i+1).'</h3>    
                       <div class="form-group col-xs-12" style="padding-left:0px;">
                         <label for="to" style="padding-right:10px; margin-bottom:0px;"><h4>To:</h4></label>
                         <input type="text" class="form-control" id="to" name="payload['.$i.'].to" placeholder="someone@example.com" style="width:100%; max-width:450px;">
